7e36c3452c553e32d8cc7035b93d35d90d02d187
Ashwini Paraye <104343839+AshwiniParaye1@users.noreply.github.com>
2024-05-17 12:08:26 +0530

    some changes

--- 02variables/main.go
+++ 02variables/main.go
@@ -8,38 +8,42 @@ import "fmt"
 var PubVariableToken = "ghghghghhhhh" // public
 
 func main() {
-	fmt.Println("Hello its day 2")
+	// fmt.Println("Hello its day 2")
 
-	//string
-	var username string = "Ashwini"
-	fmt.Println(username)
-	fmt.Printf("variable is of type: %T \n", username)
+	// //string
+	// var username string = "Ashwini"
+	// fmt.Println(username)
+	// fmt.Printf("variable is of type: %T \n", username)
 
-	//bool
-	var isBoolVar bool = true
-	fmt.Println(isBoolVar)
-	fmt.Printf("variable is of type: %T \n", isBoolVar)
+	// //bool
+	// var isBoolVar bool = true
+	// fmt.Println(isBoolVar)
+	// fmt.Printf("variable is of type: %T \n", isBoolVar)
 
-	//unit
-	var smallVal uint8 = 255
-	fmt.Println(smallVal)
-	fmt.Printf("variable is of type: %T \n", smallVal)
+	// //unit
+	// var smallVal uint8 = 255
+	// fmt.Println(smallVal)
+	// fmt.Printf("variable is of type: %T \n", smallVal)
 
-	//float
-	var smallFloat float64 = 256.123456789121212
-	fmt.Println(smallFloat)
-	fmt.Printf("variable is of type: %T \n", smallFloat)
+	// //float
+	// var smallFloat float64 = 256.123456789121212
+	// fmt.Println(smallFloat)
+	// fmt.Printf("variable is of type: %T \n", smallFloat)
 
 	//default values and some alises
-	var anotherIntVar int
-	fmt.Println(anotherIntVar)
-	fmt.Printf("variable is of type: %T \n", anotherIntVar)
+	// var anotherIntVar int
+	// fmt.Println(anotherIntVar)
+	// fmt.Printf("variable is of type: %T \n", anotherIntVar)
 
-	var anotherStrVar string
-	fmt.Println(anotherStrVar)
-	fmt.Printf("variable is of type: %T \n", anotherStrVar)
+	// var anotherStrVar string
+	// fmt.Println(anotherStrVar)
+	// fmt.Printf("variable is of type: %T \n", anotherStrVar)
 
-	fmt.Println(PubVariableToken)
-	fmt.Printf("variable is of type: %T \n", PubVariableToken)
+	var anotherFloatVar float64
+	fmt.Println(anotherFloatVar)
+	fmt.Printf("variable is of type: %T \n", anotherFloatVar)
+
+	// fmt.Println(PubVariableToken)
+	// fmt.Printf("variable is of type: %T \n", PubVariableToken)
 
 }
