ec4b8d3a55df7c2c751206355c641e45b3f21918
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-06-11 20:01:12 +0530

    get all controller

--- 25MongoApi/controller/controllers.go
+++ 25MongoApi/controller/controllers.go
@@ -2,8 +2,10 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/AshwiniParaye1/mongoApi/model"
 	"go.mongodb.org/mongo-driver/bson"
@@ -136,4 +138,12 @@ func getAllMovies() []primitive.M {
 	defer cursor.Close(context.Background())
 
 	return movies
+}
+
+// actual controllers - file
+
+func GetMyAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content Type", "application/x-www-form-urlencode")
+	allMovies := getAllMovies()
+	json.NewEncoder(w).Encode(allMovies)
 }
